bff71d90456c405f180cfdab89d8840c6c9487ac
ProjectCleverWeb <projectcleverweb@gmail.com>
2016-07-12 16:47:02 -0400

    gave hsl class array access

--- src/hsl.php
+++ src/hsl.php
@@ -4,17 +4,38 @@
 namespace projectcleverweb\color;
 
 
-class hsl {
+class hsl implements \ArrayAccess {
 	
 	private $hsl;
-	private $h;
-	private $s;
-	private $l;
 	protected $accuracy;
 	
 	public function __construct(array $rgb_array, int $accuracy = 3) {
 		$this->accuracy = $accuracy;
 		$this->hsl      = generate::rgb_to_hsl($rgb_array['r'], $rgb_array['g'], $rgb_array['b'], $this->accuracy);
+	}
+	
+	public function offsetExists($offset) :bool {
+		return isset($this->hsl[$offset]);
+	}
+	
+	public function offsetGet($offset) {
+		if ($this->offsetExists($offset)) {
+			return $this->hsl[$offset];
+		}
+		trigger_error(sprintf('Offset "%s" does not exist', $offset));
+	}
+	
+	public function offsetSet($offset, $value) {
+		if ($this->offsetExists($offset)) {
+			return $this->hsl[$offset] = (float) $value;
+		}
+		trigger_error(sprintf('Offset "%s" cannot be set', $offset));
+	}
+	
+	public function offsetUnset($offset) {
+		if ($this->offsetExists($offset)) {
+			return $this->hsl[$offset] = $value;
+		}
 	}
 }
 
