2e4981bed0655a862e9448e63c8a243b6534a87f
ProjectCleverWeb <projectcleverweb@gmail.com>
2016-07-14 18:02:55 -0400

    added code docs to regulate class; regulate::standardize_array() now forces $keys to exist, and will auto convert indexed arrays with $keys; fixed regulate::hex_int() from allowing up to 0x1FFFFFF; regulate::max() now forces $max to be an absolute value

--- src/regulate.php
+++ src/regulate.php
@@ -5,22 +5,47 @@ namespace projectcleverweb\color;
 
 class regulate {
 	
+	/**
+	 * Forces a float to be within the range of 0 and 100.
+	 * 
+	 * @param  float &$value The value to modify
+	 * @return void
+	 */
 	public static function percent(float &$value) {
 		$value = static::max(abs($value), 100);
 	}
 	
+	/**
+	 * Forces a float to be within the range of 0 and 255.
+	 * 
+	 * @param  float &$value The value to modify
+	 * @return void
+	 */
 	public static function rgb(int &$value) {
 		$value = static::max(abs($value), 256);
 	}
 	
+	/**
+	 * Forces and RGB array to have specific offsets, and max values.
+	 * 
+	 * @param  array &$rgb_array The RGB array to modify
+	 * @return void
+	 */
 	public static function rgb_array(array &$rgb_array) {
-		static::standardize_array($rgb_array);
-		$rgb_array += ['r' => 0, 'g' => 0, 'b' => 0];
+		static::standardize_array($rgb_array, ['r', 'g', 'b']);
 		static::rgb($rgb_array['r']);
 		static::rgb($rgb_array['g']);
 		static::rgb($rgb_array['b']);
 	}
 	
+	/**
+	 * Forces a float to be within the range of 0 and 100 or if the offset is
+	 * 'h' 0 and 359.
+	 * 
+	 * @param  float &$value  The value to modify
+	 * @param  string $offset The offset that is being modified
+	 * @return void
+	 */
 	public static function hsl(float &$value, string $offset) {
 		if (strtolower($offset) == 'h') {
 			$value = static::max(abs($value), 359);
@@ -28,43 +53,84 @@ class regulate {
 		static::percent($value);
 	}
 	
+	/**
+	 * Forces and HSL array to have specific offsets, and max values.
+	 * 
+	 * @param  array &$hsl_array The HSL array to modify
+	 * @return void
+	 */
 	public static function hsl_array(array &$hsl_array) {
-		static::standardize_array($hsl_array);
-		$hsl_array += ['h' => 0, 's' => 0, 'l' => 0];
+		static::standardize_array($hsl_array, ['h', 's', 'l']);
 		static::hsl($hsl_array['h'], 'h');
 		static::hsl($hsl_array['s'], 's');
 		static::hsl($hsl_array['l'], 'l');
 	}
 	
+	/**
+	 * Forces a float to be within the range of 0 and 100.
+	 * 
+	 * @param  float &$value The value to modify
+	 * @return void
+	 */
 	public static function cmyk(float &$value) {
 		static::percent($value);
 	}
 	
+	/**
+	 * Forces and CMYK array to have specific offsets, and max values.
+	 * 
+	 * @param  array &$cmyk_array The CMYK array to modify
+	 * @return void
+	 */
 	public static function cmyk_array(array &$cmyk_array) {
-		static::standardize_array($cmyk_array);
-		$hsl_array += ['c' => 0, 'm' => 0, 'y' => 0, 'k' => 0];
+		static::standardize_array($cmyk_array, ['c', 'm', 'y', 'k']);
 		static::cmyk($cmyk_array['c']);
 		static::cmyk($cmyk_array['m']);
 		static::cmyk($cmyk_array['y']);
 		static::cmyk($cmyk_array['k']);
 	}
 	
+	/**
+	 * Forces hexadecimal strings to be valid, without a "#", and not to be
+	 * shorthand.
+	 * 
+	 * @param  string &$color The color string to modify
+	 * @return void
+	 */
 	public static function hex(string &$color) {
 		static::_validate_hex_str($color);
 		static::_strip_hash($color);
 		static::_expand_shorthand($color);
 	}
 	
+	/**
+	 * Forces hexadecimal integers to be within the range of 0x0 and 0xFFFFFF.
+	 * 
+	 * @param  int  &$value The value to modify
+	 * @return void
+	 */
 	public static function hex_int(int &$value) {
-		$value = static::max(abs($value), 0x1FFFFFF);
+		$value = static::max(abs($value), 0xFFFFFF);
 	}
 	
-	public static function _strip_hash(&$hex) {
+	/**
+	 * Strips the hash mark (#) off the beginning of a sting if it has one.
+	 * 
+	 * @param  string &$hex The hex string
+	 * @return void
+	 */
+	public static function _strip_hash(string &$hex) {
 		if (is_string($hex) && substr($hex, 0 ,1) == '#') {
 			$hex = substr($hex, 1);
 		}
 	}
 	
+	/**
+	 * Expand a hex strings in short notation (e.g. 000 => 000000)
+	 * 
+	 * @param  string &$hex_str The hex string to modify
+	 * @return void
+	 */
 	public static function _expand_shorthand(string &$hex_str) {
 		if (strlen($hex_str) === 3) {
 			$r = $hex_str[0];
@@ -74,8 +140,15 @@ class regulate {
 		}
 	}
 	
-	public static function _validate_hex_str(&$hex_str) {
-		if (is_string($hex_str) && preg_match('/\A#?(?:[0-9a-f]{3}|[0-9a-f]{6}|[0-9a-f]{8})\Z/i', $hex_str)) {
+	/**
+	 * Verify that the hex string is actually a hex string. If not force it to
+	 * '000000'
+	 * 
+	 * @param  string &$hex_str The hex string to modify
+	 * @return void
+	 */
+	public static function _validate_hex_str(string &$hex_str) {
+		if (is_string($hex_str) && preg_match('/\A#?(?:[0-9a-f]{3}|[0-9a-f]{6})\Z/i', $hex_str)) {
 			return;
 		}
 		// Error - Force color and trigger notice
@@ -83,15 +156,33 @@ class regulate {
 		// [todo] Trigger Error
 	}
 	
-	protected static function standardize_array(array &$array) {
-		$array = array_change_key_case($array);
+	/**
+	 * Force an array to have specific lower-case keys. If the array is an indexed
+	 * array and the keys are provided, convert it to an associative array.
+	 * 
+	 * @param  array &$array The array to be modified
+	 * @return void
+	 */
+	protected static function standardize_array(array &$array, array $keys = []) {
+		if (!empty($keys) && array_keys($array) === range(0, count($array) - 1) && count($array) == count($keys)) {
+			$array = array_combine($keys, $array);
+		} else {
+			$array = array_change_key_case($array) + array_fill_keys($keys, 0);
+		}
 	}
 	
+	/**
+	 * Absolute and divide any number so it fits between 0 and $max
+	 * 
+	 * @param  float  $number The orginal number
+	 * @param  float  $max    The maximum value $number should be
+	 * @return float          The rsulting number as a float
+	 */
 	protected static function max(float $number, float $max) :float {
+		$max = abs($max);
 		if ($number > $max) {
 			$number -= floor($number / $max) * $max;
 		}
 		return $number;
 	}
-	
 }
