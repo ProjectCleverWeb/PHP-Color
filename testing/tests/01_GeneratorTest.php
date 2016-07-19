a6ba12b7d8cf5dcfd74d0e96137840b26e5019e8
ProjectCleverWeb <projectcleverweb@gmail.com>
2016-07-19 17:12:05 -0400

    added tests for generate class

--- testing/tests/01_GeneratorTest.php
+++ testing/tests/01_GeneratorTest.php
@@ -12,8 +12,8 @@ class GeneratorTest extends unit_test {
 	 * @test
 	 */
 	public function HEX_To_RGB() {
-		foreach ($this->vars['hex_rgb'] as $hex => $rgb) {
-			$this->assertEquals(generate::hex_to_rgb($hex), $rgb);
+		foreach ($this->vars['conversions'] as $hex => $conv) {
+			$this->assertEquals(generate::hex_to_rgb($hex), $conv['rgb']);
 		}
 	}
 	
@@ -21,8 +21,8 @@ class GeneratorTest extends unit_test {
 	 * @test
 	 */
 	public function RGB_To_HEX() {
-		foreach ($this->vars['hex_rgb'] as $hex => $rgb) {
-			$this->assertEquals(generate::rgb_to_hex($rgb['r'], $rgb['g'], $rgb['b']), $hex);
+		foreach ($this->vars['conversions'] as $hex => $conv) {
+			$this->assertEquals(generate::rgb_to_hex($conv['rgb']['r'], $conv['rgb']['g'], $conv['rgb']['b']), $hex);
 		}
 	}
 	
@@ -30,56 +30,71 @@ class GeneratorTest extends unit_test {
 	 * @test
 	 */
 	public function RGB_To_CMYK() {
-		
+		foreach ($this->vars['conversions'] as $conv) {
+			$this->assertEquals(generate::rgb_to_cmyk($conv['rgb']['r'], $conv['rgb']['g'], $conv['rgb']['b']), $conv['cmyk']);
+		}
 	}
 	
 	/**
 	 * @test
 	 */
 	public function CMYK_To_RGB() {
-		
-	}
-	
-	/**
-	 * @test
-	 */
-	public function RGB_Contrast() {
-		
-	}
-	
-	/**
-	 * @test
-	 */
-	public function RGB_Invert() {
-		
-	}
-	
-	/**
-	 * @test
-	 */
-	public function YIQ_Score() {
-		
-	}
-	
-	/**
-	 * @test
-	 */
-	public function Random() {
-		
+		foreach ($this->vars['conversions'] as $conv) {
+			$this->assertEquals(generate::cmyk_to_rgb($conv['cmyk']['c'], $conv['cmyk']['m'], $conv['cmyk']['y'], $conv['cmyk']['k']), $conv['rgb']);
+		}
 	}
 	
 	/**
 	 * @test
 	 */
 	public function RGB_To_HSL() {
-		
+		foreach ($this->vars['conversions'] as $conv) {
+			$this->assertEquals(generate::rgb_to_hsl($conv['rgb']['r'], $conv['rgb']['g'], $conv['rgb']['b']), $conv['hsl']);
+		}
 	}
 	
 	/**
 	 * @test
 	 */
 	public function HSL_To_RGB() {
-		
+		foreach ($this->vars['conversions'] as $conv) {
+			$this->assertEquals(generate::hsl_to_rgb($conv['hsl']['h'], $conv['hsl']['s'], $conv['hsl']['l']), $conv['rgb']);
+		}
 	}
 	
+	/**
+	 * @test
+	 */
+	public function RGB_Contrast() {
+		$conv = $this->vars['conversions'];
+		$this->assertEquals(generate::rgb_contrast($conv['000000']['rgb']['r'], $conv['000000']['rgb']['g'], $conv['000000']['rgb']['b']), $conv['FFFFFF']['rgb']);
+	}
+	
+	/**
+	 * @test
+	 */
+	public function RGB_Invert() {
+		$conv = $this->vars['conversions'];
+		$this->assertEquals(generate::rgb_invert($conv['000000']['rgb']['r'], $conv['000000']['rgb']['g'], $conv['000000']['rgb']['b']), $conv['FFFFFF']['rgb']);
+	}
+	
+	/**
+	 * @test
+	 */
+	public function YIQ_Score() {
+		$this->assertEquals(generate::yiq_score(255, 255, 255), 255);
+		$this->assertEquals(generate::yiq_score(0, 0, 0), 0);
+	}
+	
+	/**
+	 * @test
+	 */
+	public function Random() {
+		foreach (range(1, 1000) as $i) {
+			$rgb = generate::rand();
+			$this->assertTrue($rgb['r'] >= 0 && $rgb['r'] <= 255);
+			$this->assertTrue($rgb['g'] >= 0 && $rgb['g'] <= 255);
+			$this->assertTrue($rgb['b'] >= 0 && $rgb['b'] <= 255);
+		}
+	}
 }
