650b97925538065d192e8773983c553d919faff6
ProjectCleverWeb <projectcleverweb@gmail.com>
2016-07-20 11:28:15 -0400

    added task list

--- src/modify.php
+++ src/modify.php
@@ -7,7 +7,7 @@ class modify {
 	
 	public static function rgb(color $color, string $scope, float $adjustment, bool $as_percentage, bool $set_absolute) {
 		$current         = array_combine(['red', 'green', 'blue'], $color->rgb);
-		$scope           = strtolower($scope); // [todo] Validate scope
+		$scope           = strtolower($scope);
 		$adjustment      = max(min($adjustment, 255), 0 - 255); // Force valid range
 		$adjustment      = static::_convert_to_exact($adjustment, 255, $as_percentage);
 		$current[$scope] = static::_convert_to_abs($current[$scope], $adjustment, $set_absolute, 0, 255);
@@ -16,13 +16,13 @@ class modify {
 	
 	public static function hsl(color $color, string $scope, float $adjustment, bool $as_percentage, bool $set_absolute) {
 		$current = array_combine(['hue', 'saturation', 'light'], $color->hsl);
-		$scope   = strtolower($scope); // [todo] Validate scope
+		$scope   = strtolower($scope);
 		$max     = 100;
 		if ($scope == 'hue') {
 			$max = 359;
 		}
-		$adjustment = max(min($adjustment, $max), 0 - $max); // Force valid range
-		$adjustment = static::_convert_to_exact($adjustment, $max, $as_percentage);
+		$adjustment      = max(min($adjustment, $max), 0 - $max); // Force valid range
+		$adjustment      = static::_convert_to_exact($adjustment, $max, $as_percentage);
 		$current[$scope] = static::_convert_to_abs($current[$scope], $adjustment, $set_absolute, 0, $max);
 		return static::regenerate_hsl($color, $current);
 	}
