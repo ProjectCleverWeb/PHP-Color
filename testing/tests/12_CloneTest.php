ba2a5e8c0d8ed2c27547a5dd064a9408e2e40087
Nicholas Jordon <projectcleverweb@gmail.com>
2016-09-24 13:38:04 -0400

    various minor cleanups

---
+++ testing/tests/12_CloneTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace projectcleverweb\color;
+
+/**
+ * @requires PHP 7.0
+ * @requires PHPUnit 5
+ */
+class CloneTest extends unit_test {
+	
+	/**
+	 * @test
+	 */
+	public function test() {
+		
+	}
+}
