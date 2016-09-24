ee4c9de9bae89e541a5f0508698c05ce883fe7f6
Nicholas Jordon <projectcleverweb@gmail.com>
2016-09-24 14:18:17 -0400

    started adding tests for color class

--- testing/tests/09_ColorTest.php
+++ testing/tests/09_ColorTest.php
@@ -11,7 +11,24 @@ class ColorTest extends unit_test {
 	/**
 	 * @test
 	 */
-	public function test() {
-		
+	public function Get_Type() {
+		$color = $this->vars['conversions']['FF0000'];
+		foreach ($color as $type => $val) {
+			$this->assertEquals($type, color::get_type($val));
+		}
+		$this->assertEquals('error', color::get_type(new \stdClass));
+		$this->assertEquals('error', color::get_type(['invalid array']));
+	}
+	
+	/**
+	 * @test
+	 */
+	public function Import() {
+		foreach ($this->vars['conversions'] as $color) {
+			foreach ($color as $type => $val) {
+				$import = new color($val);
+				$this->assertEquals($color['rgb'], $import->rgb);
+			}
+		}
 	}
 }
