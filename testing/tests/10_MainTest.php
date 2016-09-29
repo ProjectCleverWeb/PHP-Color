f656a3bcc990f6235ba4da82611942a1df86c0ec
Nicholas Jordon <projectcleverweb@gmail.com>
2016-09-29 14:12:31 -0400

    started creating main class tests

--- testing/tests/10_MainTest.php
+++ testing/tests/10_MainTest.php
@@ -11,7 +11,15 @@ class MainTest extends unit_test {
 	/**
 	 * @test
 	 */
-	public function test() {
-		
+	public function Export_Colors() {
+		$color = $this->vars['conversions']['FF0000'];
+		$main  = new main($color['rgb']);
+		$this->assertEquals($color['rgb'] + ['a' => 100], $main->rgb());
+		$this->assertEquals($color['hsl'] + ['a' => 100], $main->hsl());
+		$this->assertEquals($color['hsb'] + ['a' => 100], $main->hsb());
+		$this->assertEquals($color['cmyk'] + ['a' => 100], $main->cmyk());
+		$this->assertEquals($color['hex'], $main->hex());
+		$this->assertEquals($color['hex'], $main->web_safe());
+		$this->assertEquals('#'.$color['hex'], $main->css());
 	}
 }
